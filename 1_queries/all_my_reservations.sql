948b65dc93ea362331d43091d3023691d6019a9e
Khadeeja59 <kkausar59@gmail.com>
2022-01-06 03:48:16 +0000

    LightBnB_WebApp

--- 1_queries/all_my_reservations.sql
+++ 1_queries/all_my_reservations.sql
@@ -1,3 +1,13 @@
+-- SELECT  properties.*, reservations.*, avg(property_reviews.rating) as average_rating
+-- FROM property_reviews
+-- JOIN reservations ON reservation_id = reservations.id
+-- JOIN properties ON property_id = properties.id
+-- WHERE propertie-reviews.guest_id = 1
+-- HAVING reservations.start_date < now()::date
+-- GROUP BY properties.id
+-- ORDER BY reservations.start_date
+-- LIMIT 10;
+
 SELECT properties.*, reservations.*, avg(rating) as average_rating
 FROM reservations
 JOIN properties ON reservations.property_id = properties.id
--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -51,3 +51,6 @@ CREATE TABLE property_reviews (
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
+
+
+
