0b2810a13b77b0c3336d41145a32768360b0c9ef
Khadeeja59 <kkausar59@gmail.com>
2022-01-06 19:21:14 +0000

    LighthouseBnB Reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -22,7 +22,7 @@ const getUserWithEmail = function(email) {
       WHERE users.email = $1`
       , [email])
       .then((result) => { 
-      console.log(result.rows[0]);
+      // console.log(result.rows[0]);
       return result.rows[0];
       })
       .catch((err) => {
@@ -79,6 +79,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
+  return pool
+    .query(`
+    SELECT properties.*, reservations.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    AND reservations.end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`
+    , [guest_id, limit])
+    .then((result) => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+    });
   return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
